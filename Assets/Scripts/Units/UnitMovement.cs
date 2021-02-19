b0c5732a32658545df15de86e6494faeaece73e8
Berke Atalay <berkeatalay@outlook.com.tr>
2021-02-19 22:31:19 +0300

    Player unit (tank) + localhost + navmesh movement on server + network transform (Playable on local)

---
+++ Assets/Scripts/Units/UnitMovement.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.AI;
+using Mirror;
+using UnityEngine.InputSystem;
+
+public class UnitMovement : NetworkBehaviour
+{
+
+
+    [SerializeField] NavMeshAgent agent = null;
+
+    private Camera mainCamera;
+
+    #region Server
+
+    [Command]
+    private void CmdMove(Vector3 position)
+    {
+        if (!NavMesh.SamplePosition(position, out NavMeshHit hit, 1f, NavMesh.AllAreas)) return;
+
+        agent.SetDestination(position);
+    }
+    #endregion
+
+    #region Client
+
+    public override void OnStartAuthority()
+    {
+        mainCamera = Camera.main;
+    }
+
+    [ClientCallback]
+    private void Update()
+    {
+        if (!hasAuthority) return;
+
+        if (!Mouse.current.rightButton.wasPressedThisFrame) return;
+
+        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity)) return;
+
+        CmdMove(hit.point);
+    }
+    #endregion
+
+}
